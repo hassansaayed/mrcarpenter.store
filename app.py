2b6b25b62ef3351981e2f94cc98c1cda50301022
hassansaayed <48135298+hassansaayed@users.noreply.github.com>
2025-05-16 14:25:31 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
     links = db.relationship('Link', backref='owner', lazy=True)
 
 class Link(db.Model):
